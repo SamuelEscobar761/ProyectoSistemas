861ff4b45d86d3b2201d71b1eb70845510be0525
rafaelz105 <rafaelgutierrez2303@gmail.com>
2021-07-04 03:50:52 -0400

    layout-login

---
+++ app/src/main/java/com/example/newsistemasproyecto/Home_navegation.kt
@@ -0,0 +1,11 @@
+package com.example.newsistemasproyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home_navegation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_navegation)
+    }
+}
